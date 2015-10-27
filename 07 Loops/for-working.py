22e34f75a43fb402714bc383467bbd094cb85f82
Radizzt <Richardthienhuynh@gmail.com>
2015-10-27 10:47:36 -0400

    finished Loop chapter.

--- 07 Loops/for-working.py
+++ 07 Loops/for-working.py
@@ -4,8 +4,9 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    fh = open('lines.txt')
-    for line in fh.readlines():
-        print(line, end='');
+    s= "this is a string";
+    for i, c in enumerate(s):
+        if c == 's':
+            print("index {} is an s {}".format(i));
 
 if __name__ == "__main__": main()
--- 07 Loops/loopcontrol-working.py
+++ 07 Loops/loopcontrol-working.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# for.py by Bill Weinman [http://bw.org/]
+# break.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    fh = open('lines.txt')
-    for line in fh.readlines():
-        print(line, end='');
+    s = 'this is a string'
+    for c in s:
+        print(c, end='')
 
 if __name__ == "__main__": main()
